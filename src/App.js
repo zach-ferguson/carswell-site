f6fde3e54382d0c793138ffd5cd7d7ac9462b582
zach ferguson <yoshizachz@gmail.com>
2022-02-03 21:53:36 -0500

    unused import warnings

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import { Box } from '@mui/system';
 import './App.css';
 import Navbar from './Components/Navbar';
 import PhotoCredit from './Components/PhotoCredit';
-import Home from './Pages/Home';
 import TestModal from './Components/TestModal';
-import { Card, CardMedia } from '@mui/material';
+import { CardMedia } from '@mui/material';
 import BackgroundSlider from 'react-background-slider'
 import image1 from './images/IMG_2725.jpg'
 import image2 from './images/IMG_2737.jpg'
 import image3 from './images/IMG_2829.jpg'
 import image4 from './images/IMG_2832.jpg'
 import image5 from './images/IMG_2849.jpg'
-
 
 function App() {
 
@@ -42,7 +40,7 @@ function App() {
             duration={10} transition={2} />
         </Box>
         <CardMedia sx={{ position: 'fixed',  width: '400px', top: '50%', left: '50%', marginLeft: '-200px', marginTop: '-225px'}}>
-          <iframe style={{ borderRadius: '12px', opacity: '.9' }} src="https://open.spotify.com/embed/playlist/7whoqO8hn717qz31qJ61mz?utm_source=generator&theme=0" width='100%' height='450px' allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" />
+          <iframe title="Produced by Chris Carswell on Spotify" style={{ borderRadius: '12px', opacity: '.9' }} src="https://open.spotify.com/embed/playlist/7whoqO8hn717qz31qJ61mz?utm_source=generator&theme=0" width='100%' height='450px' allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" />
         </CardMedia>
       <Box sx={{ position: 'fixed', bottom: 0, width: '100%', zIndex: 100, ml: '1rem', mb: '1rem'}}>
         <PhotoCredit sx={{  bottom: 0 }} />
